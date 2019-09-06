77e93b60b8ba55b4b218b7b1ab9b12a23594d232
asad.ali <asadali.syne@gmail.com>
2019-09-06 13:20:26 +0530

    + null handling

--- kyc/src/main/java/co/yabx/kyc/app/util/EncoderDecoderUtil.java
+++ kyc/src/main/java/co/yabx/kyc/app/util/EncoderDecoderUtil.java
@@ -6,21 +6,29 @@ import java.util.Base64;
 public class EncoderDecoderUtil {
 
 	public static String base64Encode(String data) {
-		if (data != null)
+		if (data != null && !data.isEmpty())
 			return new String(base64Encode(data.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
 		else
 			return data;
 	}
 
 	public static byte[] base64Encode(byte[] byteArray) {
-		return Base64.getEncoder().encode(byteArray);
+		if (byteArray != null) {
+			return Base64.getEncoder().encode(byteArray);
+		}
+		return byteArray;
 	}
 
 	public static String base64Decode(String str) {
-		return new String(base64Decode(str.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+		if (str != null)
+			return new String(base64Decode(str.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+		return str;
 	}
 
 	public static byte[] base64Decode(byte[] byteArray) {
-		return Base64.getDecoder().decode(byteArray);
+		if (byteArray != null && byteArray.length != 0) {
+			return Base64.getDecoder().decode(byteArray);
+		}
+		return byteArray;
 	}
 }
