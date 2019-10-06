73d4fd38487325b023f25f167bb21fe8cfa58766
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2019-10-06 17:38:02 +0530

    + auth info

--- kyc/src/main/java/co/yabx/kyc/app/entities/AuthInfo.java
+++ kyc/src/main/java/co/yabx/kyc/app/entities/AuthInfo.java
@@ -55,6 +55,18 @@ public class AuthInfo implements Serializable, UserDetails {
 	@Column(length = 512)
 	private String password;
 
+	@Column(name = "enabled", columnDefinition = "tinyint(1) default 1")
+	private boolean enabled;
+
+	@Column(name = "credentials_non_expired", columnDefinition = "tinyint(1) default 1")
+	private boolean credentialsNonExpired;
+
+	@Column(name = "account_non_expired", columnDefinition = "tinyint(1) default 1")
+	private boolean accountNonExpired;
+
+	@Column(name = "account_non_locked", columnDefinition = "tinyint(1) default 1")
+	private boolean accountNonLocked;
+
 	@Column(name = "secret_key")
 	private String secretKey;
 
@@ -151,6 +163,10 @@ public class AuthInfo implements Serializable, UserDetails {
 		this.updatedAt = updatedAt;
 	}
 
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
@@ -187,6 +203,18 @@ public class AuthInfo implements Serializable, UserDetails {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+
+	public void setAccountNonExpired(boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
+	public void setAccountNonLocked(boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
 	}
 
 }
