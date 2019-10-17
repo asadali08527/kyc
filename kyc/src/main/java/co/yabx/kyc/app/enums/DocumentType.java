e9ddb40668ee985ea36d70e50b84e317e3f050b1
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2019-10-17 18:01:45 +0530

    + Attachement enums

--- kyc/src/main/java/co/yabx/kyc/app/enums/DocumentType.java
+++ kyc/src/main/java/co/yabx/kyc/app/enums/DocumentType.java
@@ -1,6 +1,48 @@
 package co.yabx.kyc.app.enums;
 
 public enum DocumentType {
-	SIGNATURE, SELFIE, DRIVING_LICENSE, NID, PASSPORT
+	SIGNATURE, SELFIE, DRIVING_LICENSE, NATIONAL_IDENTIFICATION_NUMBER, PASSPORT, TAX_IDENTIFICATION_NUMBER,
+	VAT_REGISTRATION_NUMBER, BIRTH_CERTIFICATE, INCOME_TAX_RETURN, TRADE_LICENSE;
+
+	public static DocumentType getAddressType(String value) {
+		if (value.equalsIgnoreCase("DRIVING LICENSE")) {
+			return DocumentType.DRIVING_LICENSE;
+		} else if (value.equalsIgnoreCase("NATIONAL IDENTIFICATION NUMBER")) {
+			return DocumentType.NATIONAL_IDENTIFICATION_NUMBER;
+		} else if (value.equalsIgnoreCase("TAX IDENTIFICATION NUMBER")) {
+			return DocumentType.TAX_IDENTIFICATION_NUMBER;
+		} else if (value.equalsIgnoreCase("VAT REGISTRATION NUMBER")) {
+			return DocumentType.VAT_REGISTRATION_NUMBER;
+		} else if (value.equalsIgnoreCase("BIRTH CERTIFICATE")) {
+			return DocumentType.BIRTH_CERTIFICATE;
+		} else if (value.equalsIgnoreCase("INCOME TAX RETURN")) {
+			return DocumentType.INCOME_TAX_RETURN;
+		} else if (value.equalsIgnoreCase("TRADE LICENSE")) {
+			return DocumentType.TRADE_LICENSE;
+		} else {
+			return DocumentType.valueOf(value);
+		}
+	}
+
+	@Override
+	public String toString() {
+		if (super.toString().equals("TRADE_LICENSE")) {
+			return "TRADE LICENSE";
+		} else if (super.toString().equals("INCOME_TAX_RETURN")) {
+			return "INCOME TAX RETURN";
+		} else if (super.toString().equals("BIRTH_CERTIFICATE")) {
+			return "BIRTH CERTIFICATE";
+		} else if (super.toString().equals("VAT_REGISTRATION_NUMBER")) {
+			return "VAT REGISTRATION NUMBER";
+		} else if (super.toString().equals("TAX_IDENTIFICATION_NUMBER")) {
+			return "TAX IDENTIFICATION NUMBER";
+		} else if (super.toString().equals("NATIONAL_IDENTIFICATION_NUMBER")) {
+			return "NATIONAL IDENTIFICATION NUMBER";
+		} else if (super.toString().equals("DRIVING_LICENSE")) {
+			return "DRIVING LICENSE";
+		} else {
+			return super.toString();
+		}
+	}
 
 }
