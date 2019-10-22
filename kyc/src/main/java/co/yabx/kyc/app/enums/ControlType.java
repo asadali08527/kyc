432fc26c52c216dd8ea105f3fbd7c4b7740bfd2d
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2019-10-22 15:42:43 +0530

    + control type

--- kyc/src/main/java/co/yabx/kyc/app/enums/ControlType.java
+++ kyc/src/main/java/co/yabx/kyc/app/enums/ControlType.java
@@ -2,6 +2,17 @@ package co.yabx.kyc.app.enums;
 
 public enum ControlType {
 
-	RADIO, CHECKBOX, DROPDOWNS, TEXTAREA, BUTTON, LINK, ATTACHMENT, TEXT,CALENDER
+	RADIO("RADIO"), CHECKBOX("CHECKBOX"), DROPDOWNS("DROPDOWNS"), TEXTAREA("TEXTAREA"), BUTTON("BUTTON"), LINK("LINK"),
+	ATTACHMENT("ATTACHMENT"), TEXT("TEXT"), CALENDER("CALENDER"), DDMMYYYY("dd/mm/yyyy");
+
+	private String value;
+
+	private ControlType(String value) {
+		this.value = value;
+	}
+
+	public String toString() {
+		return this.value;
+	}
 
 }
