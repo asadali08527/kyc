f1eece03b43834f7d35f10e9292a80396715541c
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2019-11-26 15:59:48 +0530

    + null check

--- kyc/src/main/java/co/yabx/kyc/app/service/impl/EmailServiceImpl.java
+++ kyc/src/main/java/co/yabx/kyc/app/service/impl/EmailServiceImpl.java
@@ -60,17 +60,21 @@ public class EmailServiceImpl implements EmailService {
 		try {
 			Map<String, Object> variables = new HashMap<>();
 			variables.put("otp", otp.getOtp());
-			if (ActionType.Verify == actionType)
-				mailSource.sendMailTLS(mail, appConfigService.getProperty("VERIFY_EMAIL_SUBJECT", "Verify Email!"),
-						appConfigService.getProperty("VERIFY_EMAIL_TEMPLATE_PATH", "D:\\templates\\")
-								+ "/mail_templates",
-						appConfigService.getProperty("VERIFY_EMAIL_TEMPLATE_FILE_NAME", "verify_email.ftl"), variables);
-			else
-				mailSource.sendMailTLS(mail, appConfigService.getProperty("LOGIN_OTP_EMAIL_SUBJECT", "Login OTP!"),
-						appConfigService.getProperty("LOGIN_OTP_EMAIL_TEMPLATE_PATH", "D:\\templates\\")
-								+ "/mail_templates",
-						appConfigService.getProperty("LOGIN_OTP_EMAIL_TEMPLATE_FILE_NAME", "login_mail_templates.ftl"),
-						variables);
+			if (mail != null && !mail.isEmpty()) {
+				if (ActionType.Verify == actionType)
+					mailSource.sendMailTLS(mail, appConfigService.getProperty("VERIFY_EMAIL_SUBJECT", "Verify Email!"),
+							appConfigService.getProperty("VERIFY_EMAIL_TEMPLATE_PATH", "D:\\templates\\")
+									+ "/mail_templates",
+							appConfigService.getProperty("VERIFY_EMAIL_TEMPLATE_FILE_NAME", "verify_email.ftl"),
+							variables);
+				else
+					mailSource.sendMailTLS(mail, appConfigService.getProperty("LOGIN_OTP_EMAIL_SUBJECT", "Login OTP!"),
+							appConfigService.getProperty("LOGIN_OTP_EMAIL_TEMPLATE_PATH", "D:\\templates\\")
+									+ "/mail_templates",
+							appConfigService.getProperty("LOGIN_OTP_EMAIL_TEMPLATE_FILE_NAME",
+									"login_mail_templates.ftl"),
+							variables);
+			}
 		} catch (IOException | TemplateException | MessagingException ex) {
 			LOGGER.error("Exception while sending OTP to email={},error={}", mail, ex.getMessage());
 		}
