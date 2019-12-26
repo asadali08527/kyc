040a882350b310c7e6ade1bfa0e756ae6f41859e
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2019-12-26 11:14:33 +0530

    + kyc status

--- kyc/src/main/java/co/yabx/kyc/app/enums/KycStatus.java
+++ kyc/src/main/java/co/yabx/kyc/app/enums/KycStatus.java
@@ -1,11 +1,20 @@
 package co.yabx.kyc.app.enums;
 
 public enum KycStatus {
-	YES, NO, REJECTED, NEW, SUBMITTED, APPROVED, IN_PROGRESS;
+	YES, NO, REJECTED, NEW, SUBMITTED, APPROVED, IN_PROGRESS, LOC_ISSUED, LOC_GENERATED, REVIEW_INITIATED, RE_SUBMITTED;
 	public static KycStatus getAddressType(String value) {
 		if (value.equalsIgnoreCase("IN PROGRESS")) {
 			return KycStatus.IN_PROGRESS;
-		} else {
+		} else if (value.equalsIgnoreCase("IN PROGRESS")) {
+			return KycStatus.LOC_ISSUED;
+		} else if (value.equalsIgnoreCase("LOC GENERATED")) {
+			return KycStatus.LOC_GENERATED;
+		} else if (value.equalsIgnoreCase("REVIEW INITIATED")) {
+			return KycStatus.REVIEW_INITIATED;
+		} else if (value.equalsIgnoreCase("RE SUBMITTED")) {
+			return KycStatus.RE_SUBMITTED;
+		}
+		{
 			return KycStatus.valueOf(value);
 		}
 	}
@@ -14,7 +23,16 @@ public enum KycStatus {
 	public String toString() {
 		if (super.toString().equals("IN_PROGRESS")) {
 			return "In Progress";
-		} else {
+		} else if (super.toString().equals("LOC_ISSUED")) {
+			return "LO ISSUED";
+		} else if (super.toString().equals("LOC_GENERATED")) {
+			return "LOC GENERATED";
+		} else if (super.toString().equals("REVIEW_INITIATED")) {
+			return "REVIEW INITIATED";
+		} else if (super.toString().equals("RE_SUBMITTED")) {
+			return "RE SUBMITTED";
+		}
+		{
 			return super.toString();
 		}
 	}
