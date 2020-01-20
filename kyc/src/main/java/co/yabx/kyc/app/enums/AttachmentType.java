3932d7c09e98c5d29bcd08e0c2f5336c372a2bab
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2020-01-20 18:31:41 +0530

    + attachment type

--- kyc/src/main/java/co/yabx/kyc/app/enums/AttachmentType.java
+++ kyc/src/main/java/co/yabx/kyc/app/enums/AttachmentType.java
@@ -1,13 +1,15 @@
 package co.yabx.kyc.app.enums;
 
 public enum AttachmentType {
-	IdentityProof, AddressProof;
+	IdentityProof, AddressProof, DisclaimerDocument;
 
 	public static AttachmentType getAttachmentType(String value) {
 		if (value.equalsIgnoreCase("Identity Proof")) {
 			return AttachmentType.IdentityProof;
 		} else if (value.equalsIgnoreCase("Address Proof")) {
 			return AttachmentType.AddressProof;
+		} else if (value.equalsIgnoreCase("Disclaimer Document")) {
+			return AttachmentType.DisclaimerDocument;
 		} else {
 			return AttachmentType.valueOf(value);
 		}
@@ -19,6 +21,8 @@ public enum AttachmentType {
 			return "Address Proof";
 		} else if (super.toString().equals("IdentityProof")) {
 			return "Identity Proof";
+		} else if (super.toString().equals("DisclaimerDocument")) {
+			return "Disclaimer Document";
 		} else {
 			return super.toString();
 		}
