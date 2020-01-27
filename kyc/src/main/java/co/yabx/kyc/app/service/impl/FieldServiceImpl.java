af862492c3ae663cafad9a19d918efd4008e7c90
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2020-01-27 20:32:23 +0530

    + license details mutaion inside factory

--- kyc/src/main/java/co/yabx/kyc/app/service/impl/FieldServiceImpl.java
+++ kyc/src/main/java/co/yabx/kyc/app/service/impl/FieldServiceImpl.java
@@ -240,12 +240,11 @@ public class FieldServiceImpl implements FieldService {
 				if (businessDetails != null && businessDetails.getId() != null)
 					licenseDetails = licenseDetailsRepository.findByBusinessDetailsAndLicenseType(businessDetails,
 							licenseType);
-				if (licenseDetails == null) {
-					licenseDetails = new LicenseDetails();
-				}
-				licenseDetails.setLicenseType(licenseType);
 				licenseDetails = prepareLicenseDetails(appDynamicFieldsDTOList, licenseDetails);
-				licenseDetailsSet.add(licenseDetails);
+				if(licenseDetails!=null) {
+					licenseDetails.setLicenseType(licenseType);
+					licenseDetailsSet.add(licenseDetails);
+				}
 				LOGGER.info("License details={} is being added for licenseType={}", licenseDetailsSet, licenseType);
 				// businessDetails.setLicenseDetails(licenseDetailsSet);
 			}
