93e7a3175293571cfdb01e6d953b56d79ed7bf69
Asad.ali <Asad.ali@MCGL-6747.COMVIVA.COM>
2020-01-27 13:40:03 +0530

    + text templates

---
+++ kyc/src/main/java/co/yabx/kyc/app/entities/TextTemplates.java
@@ -0,0 +1,74 @@
+package co.yabx.kyc.app.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "text_templates", uniqueConstraints = { @UniqueConstraint(columnNames = { "templateName", "locale" }) })
+public class TextTemplates implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String templateName;
+
+	private String template;
+
+	private Date updatedTime;
+
+	private String locale;
+
+	@PrePersist
+	@PreUpdate
+	public void persistDate() {
+		updatedTime = new Date();
+	}
+
+	public String getTemplateName() {
+		return templateName;
+	}
+
+	public void setTemplateName(String templateName) {
+		this.templateName = templateName;
+	}
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
+	/**
+	 * @return the locale
+	 */
+	public String getLocale() {
+		return locale;
+	}
+
+	/**
+	 * @param locale the locale to set
+	 */
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+}
---
+++ kyc/src/main/java/co/yabx/kyc/app/repositories/TextTemplatesRepository.java
@@ -0,0 +1,15 @@
+package co.yabx.kyc.app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import co.yabx.kyc.app.entities.TextTemplates;
+
+@Repository("textTemplatesRepository")
+public interface TextTemplatesRepository extends JpaRepository<TextTemplates, Long> {
+	TextTemplates findByTemplateName(String templateName);
+
+	@Query("select textTemplate from TextTemplates textTemplate where locale=?1 and templateName=?2")
+	TextTemplates findByLocale(String locale, String templateName);
+}
